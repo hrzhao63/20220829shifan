947364af9a0149d5838f7bf1b3dce49ccbd46c9e
hrzhao63 <1187156063@qq.com>
2022-08-29 17:25:52 +0800

    首页与轮播图

--- firstindex.js
+++ firstindex.js
@@ -1,23 +1,26 @@
-window.onload = function() {
-    var div = document.getElementsByClassName('con_ul_li_div')
-    console.log(div)
-    var divList = div.children
-    var divListLength = divList.length
-    console.log(divListLength)
-    var count = 0
+window.onload = function () {
+    var div = document.getElementsByClassName("con_ul_li_div")[0];
+    console.dir(div);
+    var divList = div.children;
+    var divListLength = divList.length;
+    console.log(divListLength);
+    var count = 0;
     setInterval(() => {
-        if (count + 1 == divListLength) {
-            div.scrollTop = 0
-        } else {
-            count ++
-            let top = count * 200
-            var coutBanner = 0
-            for (let i = ((count - 1) * 200) + 10; i <= top; i += 10) {
-                coutBanner ++
-                setTimeout(() => {
-                    div.scrollTop = i
-                }, coutBanner * 10)
-            }
-        }        
-    }, 3000)
-}
+      if (count + 1 == divListLength) {
+        div.scrollTop = 0;
+        count = 0;
+      } else {
+        count++;
+        let top = count * 200;
+        var coutBanner = 0;
+  
+        for (let i =((count-1)*200)+ 10; i <= top; i++) {
+          coutBanner++;
+          setTimeout(() => {
+            div.scrollTop = i;
+          }, coutBanner * 10);
+        }
+        div.scrollTop = top;
+      }
+    }, 3000);
+  };
